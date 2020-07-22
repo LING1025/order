580851c7aac8fc5c24c042de0f093c855d22e83b
justone <2274104495@qq.com>
2020-07-22 15:33:00 +0800

    MyMapper通用层，common

--- provider/provider-user-service/src/main/java/com/funtl/myshop/plus/provider/mapper/AspnetUsersMapper.java
+++ provider/provider-user-service/src/main/java/com/funtl/myshop/plus/provider/mapper/AspnetUsersMapper.java
@@ -1,6 +1,7 @@
 package com.funtl.myshop.plus.provider.mapper;
 
 import com.funtl.myshop.plus.provider.domain.AspnetUsers;
+import tk.mybatis.mapper.MyMapper;
 
-public interface AspnetUsersMapper {
+public interface AspnetUsersMapper  extends MyMapper<AspnetUsers> {
 }
