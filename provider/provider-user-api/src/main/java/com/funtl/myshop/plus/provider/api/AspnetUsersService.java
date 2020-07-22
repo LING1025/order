615f0eaf6a9c91a47578af17df552c380a262981
justone <2274104495@qq.com>
2020-07-22 15:39:08 +0800

    获取用户接口

--- provider/provider-user-api/src/main/java/com/funtl/myshop/plus/provider/api/AspnetUsersService.java
+++ provider/provider-user-api/src/main/java/com/funtl/myshop/plus/provider/api/AspnetUsersService.java
@@ -2,6 +2,11 @@ package com.funtl.myshop.plus.provider.api;
 
 import com.funtl.myshop.plus.provider.domain.AspnetUsers;
 public interface AspnetUsersService{
-
+    /**
+     * 获取用户
+     * @param username 用户名
+     * @return
+     */
+    AspnetUsers get(String username);
 
 }
--- provider/provider-user-service/src/main/java/com/funtl/myshop/plus/provider/service/AspnetUsersServiceImpl.java
+++ provider/provider-user-service/src/main/java/com/funtl/myshop/plus/provider/service/AspnetUsersServiceImpl.java
@@ -5,10 +5,18 @@ import javax.annotation.Resource;
 import com.funtl.myshop.plus.provider.domain.AspnetUsers;
 import com.funtl.myshop.plus.provider.mapper.AspnetUsersMapper;
 import com.funtl.myshop.plus.provider.api.AspnetUsersService;
+import tk.mybatis.mapper.entity.Example;
+
 @Service
 public class AspnetUsersServiceImpl implements AspnetUsersService{
 
     @Resource
     private AspnetUsersMapper aspnetUsersMapper;
 
+    @Override
+    public AspnetUsers get(String username) {
+        Example example = new Example(AspnetUsers.class);
+        example.createCriteria().andEqualTo("username",username).andEqualTo("applicationId","73663109-DDA2-4C2D-8311-337946B5C373");
+        return aspnetUsersMapper.selectOneByExample(example);
+    }
 }
