1adb944e2d981569459782b4edbb89fb2c025787
LING1025 <2274104495@qq.com>
2020-08-06 14:24:40 +0800

    yml配置本机地址127.0.0.1

--- business/business-order/src/main/java/com/funtl/myshop/plus/business/configure/Swagger2Config.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/configure/Swagger2Config.java
@@ -61,7 +61,7 @@ public class Swagger2Config {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("移动端格上租赁 API文档")
-                .description("移动端格上租赁 api网关接口，http://192.168.168.70:9999/api")
+                .description("移动端格上租赁 api网关接口，http://127.0.0.1:9999/api")
                 .version("1.0")
                 .build();
     }
