f075866098b0482a8c2758b7db3e11311f1226a4
LING1025 <2274104495@qq.com>
2020-08-26 11:23:34 +0800

    orderBudget删除

--- provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/api/OrderBudgetService.java
+++
@@ -1,6 +0,0 @@
-package com.funtl.myshop.plus.provider.api;
-
-import com.funtl.myshop.plus.provider.domain.OrderBudget;
-public interface OrderBudgetService{
-
-}
--- provider/provider-order-service/src/main/java/com/funtl/myshop/plus/provider/mapper/OrderBudgetMapper.java
+++
@@ -1,7 +0,0 @@
-package com.funtl.myshop.plus.provider.mapper;
-
-import com.funtl.myshop.plus.provider.domain.OrderBudget;
-import tk.mybatis.mapper.MyMapper;
-
-public interface OrderBudgetMapper extends MyMapper<OrderBudget> {
-}
