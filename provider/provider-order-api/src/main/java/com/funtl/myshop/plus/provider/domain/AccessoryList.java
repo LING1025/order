e00e1f2737d8221a7670e00ef5606cfc5763cb56
LING1025 <2274104495@qq.com>
2020-09-02 15:57:04 +0800

    获取配件明细数据api添加返回属性

--- provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/domain/AccessoryList.java
+++ provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/domain/AccessoryList.java
@@ -12,10 +12,22 @@ public class AccessoryList implements Serializable {
     @ApiModelProperty(value = "厂商")
     private String supplierN;
 
+    @ApiModelProperty(value = "主配件")
+    private String mainAccessory;
+
     @ApiModelProperty(value = "次配件")
     private String subAccessory;
 
     @ApiModelProperty(value = "配件售价")
     private Integer sellingAmt;
+
+    @ApiModelProperty(value = "座位数")
+    private String seat;
+
+    @ApiModelProperty(value = "施工位置")
+    private String location;
+
+    @ApiModelProperty(value = "施工及价格变动说明")
+    private String remark;
 
 }
