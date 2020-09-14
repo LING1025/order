3f57cd81e38e20d426ebc56274667c06586ec465
LING1025 <2274104495@qq.com>
2020-09-14 09:36:02 +0800

    路由网关改为9001；用户信息接口开通测试

--- business/business-wxUser/business-wxUser-service/src/main/java/com/funtl/myshop/plus/business/configure/WebSecurityConfiguration.java
+++ business/business-wxUser/business-wxUser-service/src/main/java/com/funtl/myshop/plus/business/configure/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.funtl.myshop.plus.business.configure;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,9 +14,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     // 暂时先不用
-    /*@Override
+    @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/table/queryMode","/table/query");// ignoring是完全绕过了spring security的所有filter，相当于不走spring security
-    }*/
+                .antMatchers("/wxUser/wxInfo/{openId}");// ignoring是完全绕过了spring security的所有filter，相当于不走spring security
+    }
 }
