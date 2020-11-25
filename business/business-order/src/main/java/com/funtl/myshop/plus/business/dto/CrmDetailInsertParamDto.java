331a21237a9daa949c73aaf16547717b33b1bc8c
LING1025 <2274104495@qq.com>
2020-11-25 15:25:04 +0800

    行程记录中的数据从行程安排里获取

--- business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/CrmDetailInsertParamDto.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/CrmDetailInsertParamDto.java
@@ -1,10 +1,12 @@
 package com.funtl.myshop.plus.business.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @ApiModel(value = "CRM:行程记录打卡数据")
@@ -30,7 +32,32 @@ public class CrmDetailInsertParamDto implements Serializable {
     @ApiModelProperty(value = "具体地址")
     private String vstRealAddr;
 
+    @ApiModelProperty(value = "拜访日期yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date vstDT;
+
+    @ApiModelProperty(value = "客户名称")
+    private String customerName;
+
     @ApiModelProperty(value = "客户序号")
     private Long tradeItemAuto;
+
+    @ApiModelProperty(value = "客户来源")
+    private String custSourceName;
+
+    @ApiModelProperty(value = "客户来源序号")
+    private Integer custSource;
+
+    @ApiModelProperty(value = "活动内容")
+    private String vstTypeName;
+
+    @ApiModelProperty(value = "活动内容序号")
+    private Integer vstType;
+
+    @ApiModelProperty(value = "联系人")
+    private String contName;
+
+    @ApiModelProperty(value = "拜访地点")
+    private String vstAddr;
 
 }
--- business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/CrmDetailParamDto.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/CrmDetailParamDto.java
@@ -15,36 +15,8 @@ public class CrmDetailParamDto implements Serializable {
     @ApiModelProperty(value = "报告序号")
     private Long rptVstAuto=0L;
 
-    @ApiModelProperty(value = "实际拜访日期yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private Date vstDT;
-
-    @ApiModelProperty(value = "客户名称")
-    private String customerName;
-
-    @ApiModelProperty(value = "客户序号")
-    private Long tradeItemAuto;
-
-    @ApiModelProperty(value = "客户来源")
-    private String custSourceName;
-
-    @ApiModelProperty(value = "客户来源序号")
-    private Integer custSource;
-
-    @ApiModelProperty(value = "活动内容")
-    private String vstTypeName;
-
-    @ApiModelProperty(value = "活动内容序号")
-    private Integer vstType;
-
     @ApiModelProperty(value = "产生总费用")
     private BigDecimal fee;
-
-    @ApiModelProperty(value = "联系人")
-    private String contName;
-
-    @ApiModelProperty(value = "拜访地点")
-    private String vstAddr;
 
     @ApiModelProperty(value = "进度")
     private String outComingName;
