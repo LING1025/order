fffeef5d1114a63aa242eb3f60bfa904e0e105be
LING1025 <2274104495@qq.com>
2021-01-19 13:28:03 +0800

    经纬度转地址未完成

---
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/controller/CarApplyController.java
@@ -0,0 +1,12 @@
+package com.funtl.myshop.plus.business.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "车队管理模块相关操作")
+@RestController
+@RequestMapping(value = "carApply")
+public class CarApplyController {
+
+}
