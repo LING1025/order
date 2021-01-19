4410a94bc1455ac9bd3d1b672b93b5e019732839
LING1025 <2274104495@qq.com>
2021-01-19 13:38:23 +0800

    经纬度转地址api已完成

--- business/business-order/src/main/java/com/funtl/myshop/plus/business/controller/CarApplyController.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/controller/CarApplyController.java
@@ -1,12 +1,42 @@
 package com.funtl.myshop.plus.business.controller;
 
+import com.funtl.myshop.plus.business.dto.GetLocation;
+import com.funtl.myshop.plus.commons.dto.ResponseResult;
+import com.funtl.myshop.plus.commons.utils.MapperUtils;
+import com.funtl.myshop.plus.controller.LocationUtils;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 
 @Api(tags = "车队管理模块相关操作")
 @RestController
 @RequestMapping(value = "carApply")
 public class CarApplyController {
+    @ApiOperation(value = "经纬度转地址")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "lng", value = "经度", required = true, dataType = "String", paramType = "path"),
+            @ApiImplicitParam(name = "lat", value = "纬度", required = true, dataType = "String", paramType = "path")
+    })
+    @GetMapping(value = "queryLocation")
+    public ResponseResult<GetLocation> queryLocation(@RequestParam(name = "lng")String lng,
+                                                        @RequestParam(name = "lat")String lat) throws Exception {
+        Map<String, Object> map = LocationUtils.getLocation(lng, lat);
+        GetLocation getLocation = new GetLocation();
+        getLocation.setProvince((String) map.get("province"));
+        getLocation.setNation((String) map.get("nation"));
+        getLocation.setCity((String) map.get("city"));
+        getLocation.setProvinceCode((String) map.get("provinceCode"));
+        getLocation.setCityCode((String) map.get("cityCode"));
+        getLocation.setNationCode((String) map.get("nationCode"));
+        return new ResponseResult<>(ResponseResult.CodeStatus.OK, "查询成功", getLocation);
+    }
 
 }
--- business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/GetLocation.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/dto/GetLocation.java
@@ -1,4 +1,29 @@
 package com.funtl.myshop.plus.business.dto;
 
-public class GetLocation {
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(value = "经纬度转地址数据")
+public class GetLocation implements Serializable {
+    @ApiModelProperty(value = "省份")
+    private String province;
+
+    @ApiModelProperty(value = "国籍")
+    private String nation;
+
+    @ApiModelProperty(value = "城市")
+    private String city;
+
+    @ApiModelProperty(value = "省份代码")
+    private String provinceCode;
+
+    @ApiModelProperty(value = "城市代码")
+    private String cityCode;
+
+    @ApiModelProperty(value = "国家代码")
+    private String nationCode;
 }
