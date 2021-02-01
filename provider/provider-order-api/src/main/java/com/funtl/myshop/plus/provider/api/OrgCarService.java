e99e171f12b39d05f8c0f053dde83ece03c0b100
LING1025 <2274104495@qq.com>
2021-02-01 18:05:05 +0800

    用车申请：自动配车、用车审核：获取车辆列表未完成

--- provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/api/OrgCarService.java
+++ provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/api/OrgCarService.java
@@ -1,7 +1,18 @@
 package com.funtl.myshop.plus.provider.api;
 
 import com.funtl.myshop.plus.provider.domain.OrgCar;
+import com.funtl.myshop.plus.provider.domain.OrgCarList;
+import com.funtl.myshop.plus.provider.dto.OrgCarQueryParam;
+
+import java.util.List;
+
 public interface OrgCarService{
 
+    /**
+     * 用车申请：自动配车、用车审核：获取车辆列表
+     * @param orgCarQueryParam
+     * @return
+     */
+    List<OrgCarList> selectOrgCar(OrgCarQueryParam orgCarQueryParam);
 
 }
--- provider/provider-order-service/src/main/java/com/funtl/myshop/plus/provider/service/OrgCarServiceImpl.java
+++ provider/provider-order-service/src/main/java/com/funtl/myshop/plus/provider/service/OrgCarServiceImpl.java
@@ -1,10 +1,15 @@
 package com.funtl.myshop.plus.provider.service;
 
 import javax.annotation.Resource;
+
+import com.funtl.myshop.plus.provider.domain.OrgCarList;
+import com.funtl.myshop.plus.provider.dto.OrgCarQueryParam;
 import com.funtl.myshop.plus.provider.mapper.OrgCarMapper;
 import com.funtl.myshop.plus.provider.domain.OrgCar;
 import com.funtl.myshop.plus.provider.api.OrgCarService;
 import org.apache.dubbo.config.annotation.Service;
+
+import java.util.List;
 
 @Service(version = "1.0.0")
 public class OrgCarServiceImpl implements OrgCarService{
@@ -12,4 +17,8 @@ public class OrgCarServiceImpl implements OrgCarService{
     @Resource
     private OrgCarMapper orgCarMapper;
 
+    @Override
+    public List<OrgCarList> selectOrgCar(OrgCarQueryParam orgCarQueryParam) {
+        return orgCarMapper.selectOrgCar(orgCarQueryParam);
+    }
 }
