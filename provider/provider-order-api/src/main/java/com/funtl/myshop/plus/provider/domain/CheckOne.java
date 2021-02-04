15411b35bf118318f158079ca225416af313465e
LING1025 <2274104495@qq.com>
2021-02-04 12:01:56 +0800

    选取审核明细未完成

---
+++ provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/domain/CheckOne.java
@@ -0,0 +1,12 @@
+package com.funtl.myshop.plus.provider.domain;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@ApiModel(value = "用车审核：选取审核明细")
+public class CheckOne implements Serializable {
+
+}
