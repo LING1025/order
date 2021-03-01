53ced4b3c173913da8f84c5717fc8433bf3008d1
LING1025 <2274104495@qq.com>
2021-03-01 16:16:48 +0800

    多个申请列表查询：申请时间范围修改

--- provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/dto/UseCarQueryParam.java
+++ provider/provider-order-api/src/main/java/com/funtl/myshop/plus/provider/dto/UseCarQueryParam.java
@@ -24,12 +24,12 @@ public class UseCarQueryParam implements Serializable {
 
     private String makNo;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "GMT+8")//把时间类型 转换成JSON格式类型，前提取出进行展示
+    @JsonFormat(pattern = "yyyy-MM-dd")//把时间类型 转换成JSON格式类型，前提取出进行展示
 //    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")//会把字符串类型 按照格式yyyy-MM-dd HH:mm:ss 转换成时间类型
     private String planStartDT;
 
 //    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private String planEndDT;
 
     private String statusN;
