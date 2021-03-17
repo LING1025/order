a411dfb58ebb7e18b2b3df243a8ee24a17f2560d
LING1025 <2274104495@qq.com>
2021-03-17 13:26:21 +0800

    请款相关删除

--- provider/provider-order-service/src/main/java/com/funtl/myshop/plus/provider/mapper/PurchaseMapper.java
+++
@@ -1,8 +0,0 @@
-package com.funtl.myshop.plus.provider.mapper;
-
-import com.funtl.myshop.plus.provider.domain.Purchase;
-import tk.mybatis.mapper.MyMapper;
-
-public interface PurchaseMapper extends MyMapper<Purchase> {
-
-}
