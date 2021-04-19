9c76ba50e646b5c43d8e2a11fde7819cde2f10a4
LING1025 <2274104495@qq.com>
2021-04-19 14:35:38 +0800

    读取logNew文件

--- business/business-wxUser/business-wxUser-service/src/main/java/com/funtl/myshop/plus/business/controller/TBoxLogController.java
+++ business/business-wxUser/business-wxUser-service/src/main/java/com/funtl/myshop/plus/business/controller/TBoxLogController.java
@@ -4,21 +4,47 @@ import com.funtl.myshop.plus.business.BusinessStatus;
 import com.funtl.myshop.plus.business.dto.FileInfo;
 import com.funtl.myshop.plus.commons.dto.ResponseResult;
 import com.funtl.myshop.plus.controller.TBoxLog;
+import com.google.common.collect.Lists;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 @Api(tags = "TBox相关操作")
 @RestController
 @RequestMapping(value = "TBox")
 public class TBoxLogController {
 
-    @ApiOperation(value = "将LogNew数据插入到数据库")
+    @ApiOperation(value = "将logNew数据插入到数据库")
     @PostMapping(value = "logNew")
-    public ResponseResult<String> insert(){
-        TBoxLog.getTBoxLog();
-        return new ResponseResult<>(BusinessStatus.OK.getCode(), "保存成功", null);
+    public ResponseResult<List<String>> insert(){
+//        TBoxLog.getTBoxLog();
+        //todo:解析logNew文件插入数据库
+        List<String> list = Lists.newArrayList();
+        String line = null;
+        File f = new File("D:\\twoWork\\logNew.txt");
+        try{
+            // 读取“log.txt”文件
+            FileReader fileReader = new FileReader(f);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                String str = line.split(",")[2];
+                System.out.println(str);
+                if (str.equals("01") || str.equals("24")){
+                    list.add(line);
+                }
+            }
+            return new ResponseResult<>(BusinessStatus.OK.getCode(), "保存成功", list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseResult<>(BusinessStatus.FAIL.getCode(), "保存失败", null);
     }
 }
