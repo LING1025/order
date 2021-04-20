3358b46ccd7831abc7512925ebb847927bae09b1
LING1025 <2274104495@qq.com>
2021-04-20 10:09:47 +0800

    批量插入设备记录信息未完成

--- provider/provider-car-api/src/main/java/com/funtl/myshop/plus/provider/service/TBoxLogService.java
+++ provider/provider-car-api/src/main/java/com/funtl/myshop/plus/provider/service/TBoxLogService.java
@@ -1,7 +1,15 @@
 package com.funtl.myshop.plus.provider.service;
 
 import com.funtl.myshop.plus.provider.domain.TBoxLog;
+
+import java.util.List;
+
 public interface TBoxLogService{
 
-
+    /**
+     * 批量插入设备记录信息
+     * @param tBoxLogList
+     * @return
+     */
+    Integer insertList(List<TBoxLog> tBoxLogList);
 }
--- provider/provider-car-service/src/main/java/com/funtl/myshop/plus/provider/service/TBoxLogServiceImpl.java
+++ provider/provider-car-service/src/main/java/com/funtl/myshop/plus/provider/service/TBoxLogServiceImpl.java
@@ -6,10 +6,16 @@ import com.funtl.myshop.plus.provider.mapper.TBoxLogMapper;
 import com.funtl.myshop.plus.provider.service.TBoxLogService;
 import org.apache.dubbo.config.annotation.Service;
 
+import java.util.List;
+
 @Service(version = "1.0.0")
 public class TBoxLogServiceImpl implements TBoxLogService{
 
     @Resource
     private TBoxLogMapper tBoxLogMapper;
 
+    @Override
+    public Integer insertList(List<TBoxLog> tBoxLogList) {
+        return tBoxLogMapper.insertList(tBoxLogList);
+    }
 }
