1e513da8ed2e7d98779b04440cd2cc588980f574
LING1025 <2274104495@qq.com>
2021-06-10 17:20:26 +0800

    付款日修改审核未完成

--- business/business-order/src/main/java/com/funtl/myshop/plus/business/controller/OrdersPayController.java
+++ business/business-order/src/main/java/com/funtl/myshop/plus/business/controller/OrdersPayController.java
@@ -1,18 +1,18 @@
 package com.funtl.myshop.plus.business.controller;
 
 import com.funtl.myshop.plus.business.BusinessException;
+import com.funtl.myshop.plus.business.dto.OrdersPayParamDto;
 import com.funtl.myshop.plus.commons.dto.ResponseResult;
 import com.funtl.myshop.plus.provider.api.OrdersService;
 import com.funtl.myshop.plus.provider.domain.OrdersPayList;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Api(tags = "付款日修改审核相关操作")
@@ -34,5 +34,12 @@ public class OrdersPayController {
             return new ResponseResult<>(ResponseResult.CodeStatus.FAIL,"待签核数：0",null);
         }
         return new ResponseResult<>(ResponseResult.CodeStatus.OK,"查询成功",lists);
+    }
+
+    @ApiOperation(value = "付款日修改审核：核准、驳回")
+    @PostMapping(value = "insert")
+    public ResponseResult<String> insert(@ApiParam(value = "付款日修改审核：核准、驳回") @Valid @RequestBody OrdersPayParamDto ordersPayParamDto){
+
+        return new ResponseResult<>(ResponseResult.CodeStatus.OK, "保存成功", null);
     }
 }
