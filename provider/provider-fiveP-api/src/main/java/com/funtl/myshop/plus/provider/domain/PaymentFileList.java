205b86cb837c0897cbd3a78413e7275ef8bbc8fd
LING1025 <2274104495@qq.com>
2021-06-16 11:59:19 +0800

    客户汇款输入：附件列表返回属性

---
+++ provider/provider-fiveP-api/src/main/java/com/funtl/myshop/plus/provider/domain/PaymentFileList.java
@@ -0,0 +1,26 @@
+package com.funtl.myshop.plus.provider.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@ApiModel(value = "客户汇款输入：附件列表")
+public class PaymentFileList implements Serializable {
+    @ApiModelProperty(value = "文件名称")
+    private String fileName;
+
+    @ApiModelProperty(value = "附件来源")
+    private String typeName;
+
+    @ApiModelProperty(value = "上传人员")
+    private String cUsername;
+
+    @ApiModelProperty(value = "上传时间")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date cdt;
+}
