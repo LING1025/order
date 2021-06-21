f907770c25c28f1660ceb7b03577eebfe3c7f964
LING1025 <2274104495@qq.com>
2021-06-21 14:01:50 +0800

    拨款申请作业、连接了正式库

---
+++ business/business-fiveP/src/main/java/com/funtl/myshop/plus/business/controller/GrantController.java
@@ -0,0 +1,11 @@
+package com.funtl.myshop.plus.business.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "拨款申请作业相关操作")
+@RestController
+@RequestMapping(value = "grant")
+public class GrantController {
+}
